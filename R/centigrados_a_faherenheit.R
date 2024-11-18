c94b2d4ffd5c6d305ac68dca13adb364645935a6
celestedallago <celedallago@gmail.com>
2024-11-18 15:03:45 -0300

    hago test, nueva funcion

--- R/centigrados_a_faherenheit.R
+++ R/centigrados_a_faherenheit.R
@@ -1,4 +1,21 @@
+#' Convertidor de centigrados a fahrenheit
+#'
+#' @param centigrados temperatura en grados centigrados
+#'
+#' @return
+#' Temperatura en grados fahrenheit
+#'
+#'
+#' @examples
+#' centigrados_a_fahrenheit(25)
+#'
+#' @export
+#'
 centigrados_a_fahrenheit <- function(centigrados) {
+  if (!is.numeric(centigrados)) {
+    stop("El argumento centigrados debe ser numérico.")
+  }
+
   fahrenheit <- (centigrados * 9/5) + 32
   return(fahrenheit)
 }
---
+++ tests/testthat/test-centigrados_a_fahrenheit.R
@@ -0,0 +1,14 @@
+test_that("Test de la función centigrados_a_fahrenheit", {
+
+  expect_equal(centigrados_a_fahrenheit(0), 32)
+  expect_equal(centigrados_a_fahrenheit(100), 212)
+  expect_equal(centigrados_a_fahrenheit(-40), -40)
+  expect_equal(centigrados_a_fahrenheit(25), 77)
+  expect_equal(centigrados_a_fahrenheit(-17.77778), 0, tolerance = 1e-5)
+
+
+  expect_error(centigrados_a_fahrenheit("string"), "El argumento centigrados debe ser numérico.")
+  expect_error(centigrados_a_fahrenheit(TRUE), "El argumento centigrados debe ser numérico.")
+  expect_error(centigrados_a_fahrenheit(NULL), "El argumento centigrados debe ser numérico.")
+})
+
