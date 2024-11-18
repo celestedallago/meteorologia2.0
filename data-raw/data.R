788be1d80090ed6a5ea175afcf587ffd87ad253b
celestedallago <celedallago@gmail.com>
2024-11-18 00:04:22 -0300

    agrego data.r a data_raw

---
+++ data-raw/data.R
@@ -0,0 +1,16 @@
+
+url <- "https://raw.githubusercontent.com/rse-r/intro-programacion/main/datos/NH0910.csv"
+NH0910 <- read.csv(url)
+head(NH0910)
+
+url2 <- "https://raw.githubusercontent.com/rse-r/intro-programacion/main/datos/NH0910.csv"
+NH0472 <- read.csv(url2)
+head(NH0472)
+
+url3 <- "https://raw.githubusercontent.com/rse-r/intro-programacion/main/datos/NH0437.csv"
+NH0437 <- read.csv(url3)
+head(NH0437)
+
+url4 <- " https://raw.githubusercontent.com/rse-r/intro-programacion/main/datos/NH0098.csv"
+NH0098 <- read.csv(url4)
+head(NH0098)
